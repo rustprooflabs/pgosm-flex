6ecd97384ecfe89f956f886b79c5f552b082f905
Ryan Lambert <ryan@rustprooflabs.com>
2021-05-01 16:06:42 -0600

    Rework and consolidate logic for amenity inclusion. Error check for invalid bench tags #119

--- db/qc/osm_tag_suggestions.sql
+++ db/qc/osm_tag_suggestions.sql
@@ -1,10 +1,20 @@
 
 
 SELECT geom_type, osm_id,
-        'Update to: amenity=bicycle_parking per https://wiki.openstreetmap.org/wiki/Key:bicycle_parking'::TEXT AS suggestion,
+        'Update to: amenity=bicycle_parking per https://wiki.openstreetmap.org/wiki/Key:bicycle_parking'::TEXT
+            AS suggestion,
         osm_url, tags
     FROM osm.tags
     WHERE tags->>'bicycle_parking' IS NOT NULL
         AND tags->>'amenity' IS NULL
+UNION
+SELECT t.geom_type, t.osm_id,
+        'Invald bench value. Valid values for `bench` are "yes" and "no" per https://wiki.openstreetmap.org/wiki/Key:bench'::TEXT
+            AS suggestion,
+        t.osm_url, t.tags
+    FROM osm.tags t
+    WHERE t.tags->>'amenity' IS NULL
+        AND t.tags->>'bench' IS NOT NULL
+        AND t.tags->>'bench' NOT IN ('yes', 'no')
 ;
 
