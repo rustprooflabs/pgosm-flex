6454018139607d61e53a7074ad10682c6f5d183f
Ryan Lambert <rustprooflabs@users.noreply.github.com>
2023-06-05 19:38:30 -0600

    Add shop tests (#338)
    Add tests for shop point/polygon counts. Includes moving test docs to main docs page, and overall improvements to the examples.

---
+++ tests/sql/shop_point_osm_type_subtype_count.sql
@@ -0,0 +1,5 @@
+SELECT osm_type COLLATE "C", osm_subtype COLLATE "C", COUNT(*)
+    FROM osm.shop_point
+    GROUP BY osm_type COLLATE "C", osm_subtype COLLATE "C"
+    ORDER BY osm_type COLLATE "C", osm_subtype COLLATE "C"
+;
