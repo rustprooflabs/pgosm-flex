e6bbf4bf13261ef5c2972bc4f27e616a62053c9b
Ryan Lambert <ryan@rustprooflabs.com>
2023-06-05 20:58:35 -0600

    Restructure POI loading in prep to migrate all view

--- flex-config/style/poi.lua
+++ flex-config/style/poi.lua
@@ -1,126 +1,7 @@
 require "helpers"
+require "style.poi_helpers"
 
 local tables = {}
-
--- Keys to include for further checking.  Not all values from each key will be preserved
-local poi_first_level_keys = {
-    'building',
-    'shop',
-    'amenity',
-    'leisure',
-    'man_made',
-    'tourism',
-    'landuse',
-    'natural',
-    'historic'
-}
-
-local is_first_level_poi = make_check_in_list_func(poi_first_level_keys)
-
-
-function building_poi(object)
-    local bldg_name = get_name(object.tags)
-    if (bldg_name ~= '' or object.tags.operator) then
-        return true
-    end
-
-    return false
-end
-
-
-function landuse_poi(object)
-    if (object.tags.landuse == 'cemetery'
-            or object.tags.landuse == 'orchard'
-            or object.tags.landuse == 'railway'
-            or object.tags.landuse == 'village_green'
-            or object.tags.landuse == 'vineyard') then
-        return true
-    end
-
-    return false
-
-end
-
-
-function man_made_poi(object)
-    if (object.tags.man_made == 'beacon'
-            or object.tags.man_made == 'chimney'
-            or object.tags.man_made == 'communications_tower'
-            or object.tags.man_made == 'crane'
-            or object.tags.man_made == 'flagpole'
-            or object.tags.man_made == 'lighthouse'
-            or object.tags.man_made == 'mast'
-            or object.tags.man_made == 'obelisk'
-            or object.tags.man_made == 'observatory'
-            or object.tags.man_made == 'offshore_platform'
-            or object.tags.man_made == 'pier'
-            or object.tags.man_made == 'silo'
-            or object.tags.man_made == 'survey_point'
-            or object.tags.man_made == 'telescope'
-            or object.tags.man_made == 'tower'
-            or object.tags.man_made == 'water_tap'
-            or object.tags.man_made == 'water_tower'
-            or object.tags.man_made == 'water_well'
-            or object.tags.man_made == 'windmill'
-            or object.tags.man_made == 'works'
-            ) then
-        return true
-    end
-
-    return false
-end
-
-function natural_poi(object)
-    if (object.tags.natural == 'peak'
-            or object.tags.natural == 'glacier'
-            or object.tags.natural == 'reef'
-            or object.tags.natural == 'hot_spring'
-            or object.tags.natural == 'bay') then
-        return true
-    end
-
-    return false
-end
-
-
-local function get_osm_type_subtype(object)
-    local osm_type_table = {}
-
-    if object.tags.shop then
-        osm_type_table['osm_type'] = 'shop'
-        osm_type_table['osm_subtype'] = object:grab_tag('shop')
-    elseif object.tags.amenity then
-        osm_type_table['osm_type'] = 'amenity'
-        osm_type_table['osm_subtype'] = object:grab_tag('amenity')
-    elseif object.tags.building then
-        osm_type_table['osm_type'] = 'building'
-        osm_type_table['osm_subtype'] = object:grab_tag('building')
-    elseif object.tags.leisure then
-        osm_type_table['osm_type'] = 'leisure'
-        osm_type_table['osm_subtype'] = object:grab_tag('leisure')
-    elseif object.tags.landuse then
-        osm_type_table['osm_type'] = 'landuse'
-        osm_type_table['osm_subtype'] = object:grab_tag('landuse')
-    elseif object.tags.natural then
-        osm_type_table['osm_type'] = 'natural'
-        osm_type_table['osm_subtype'] = object:grab_tag('natural')
-    elseif object.tags.man_made then
-        osm_type_table['osm_type'] = 'man_made'
-        osm_type_table['osm_subtype'] = object:grab_tag('man_made')
-    elseif object.tags.tourism then
-        osm_type_table['osm_type'] = 'tourism'
-        osm_type_table['osm_subtype'] = object:grab_tag('tourism')
-    elseif object.tags.historic then
-        osm_type_table['osm_type'] = 'historic'
-        osm_type_table['osm_subtype'] = object.tags['historic']
-    else
-        -- Cannot be NULL
-        osm_type_table['osm_type'] = 'Unknown'
-        osm_type_table['osm_subtype'] = 'Unknown'
-    end
-
-    return osm_type_table
-end
 
 
 tables.poi_point = osm2pgsql.define_table({
@@ -205,25 +86,11 @@ function poi_process_node(object)
         return
     end
 
-    if (object.tags.natural and not natural_poi(object)) then
+    if not second_level_tag_check(object) then
         return
     end
 
-    if (object.tags.landuse and not landuse_poi(object)) then
-        return
-    end
-
-    if (object.tags.building and not building_poi(object)) then
-        return
-    end
-
-
-    if (object.tags.man_made and not man_made_poi(object)) then
-        return
-    end
-
-
-    local osm_types = get_osm_type_subtype(object)
+    local osm_types = get_osm_type_subtype_poi(object)
 
     local name = get_name(object.tags)
     local housenumber  = object.tags['addr:housenumber']
@@ -258,24 +125,11 @@ function poi_process_way(object)
         return
     end
 
-    -- Deeper checks for specific osm_type details
-    if (object.tags.natural and not natural_poi(object)) then
+    if not second_level_tag_check(object) then
         return
     end
 
-    if (object.tags.landuse and not landuse_poi(object)) then
-        return
-    end
-
-    if (object.tags.building and not building_poi(object)) then
-        return
-    end
-
-    if (object.tags.man_made and not man_made_poi(object)) then
-        return
-    end
-
-    local osm_types = get_osm_type_subtype(object)
+    local osm_types = get_osm_type_subtype_poi(object)
 
     local name = get_name(object.tags)
     local housenumber  = object.tags['addr:housenumber']
@@ -327,25 +181,13 @@ function poi_process_relation(object)
         return
     end
 
-    -- Deeper checks for specific osm_type details
-    if (object.tags.natural and not natural_poi(object)) then
+    if not second_level_tag_check(object) then
         return
     end
 
-    if (object.tags.landuse and not landuse_poi(object)) then
-        return
-    end
-
-    if (object.tags.building and not building_poi(object)) then
-        return
-    end
-
-    if (object.tags.man_made and not man_made_poi(object)) then
-        return
-    end
 
     -- Gets osm_type and osm_subtype
-    local osm_types = get_osm_type_subtype(object)
+    local osm_types = get_osm_type_subtype_poi(object)
 
     local name = get_name(object.tags)
     local housenumber  = object.tags['addr:housenumber']
